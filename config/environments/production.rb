414ec8e6e7fe911ddb22244c6c4b709acac421ea
leffen <leffen@gmail.com>
2013-08-15 11:27:50 +0200

    Added ENV config variables to smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,17 @@ Mcraft::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host =>ENV['MAIL_HOST'] }
+
+  config.action_mailer.smtp_settings = {
+      :address => 'smtp.mandrillapp.com',
+      :port => '587',
+      :domain => ENV['DOMAIN'],
+      :user_name => ENV['MU'],
+      :password => ENV['MA']
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
+
 end
